e9df61075a41b7c108fe7494da86392876e4549e
Sarmad Sohail <sarmad-sohail@hotmail.com>
2020-11-20 06:30:21 +1200

    register user api

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//Register
+Route::post('register', 'UserController@register')->name('user.register');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+//Login
+Route::get('login', function () {
+    abort(403);
+})->name('login');
