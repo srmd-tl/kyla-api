8016ec901bc4ffd20caa108096c155134aa4756e
Sarmad Sohail <sarmad-sohail@hotmail.com>
2020-11-20 06:11:50 +1200

    updated users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('race');
+            $table->string('gender');
+            $table->int('age');
+            $table->int('state');
             $table->rememberToken();
             $table->timestamps();
         });
