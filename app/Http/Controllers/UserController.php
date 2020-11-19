3680caa9b1f4e9cb74403ce7dc4b8cbc5d59d61d
Sarmad Sohail <sarmad-sohail@hotmail.com>
2020-11-20 06:50:01 +1200

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,6 +21,7 @@ class UserController extends Controller
                 "password" => "required"
             ]
         );
+
         $data =
             [
                 "name" => request()->name,
--- app/Providers/ResponseMacroServiceProvider.php
+++ app/Providers/ResponseMacroServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
 
 class ResponseMacroServiceProvider extends ServiceProvider
