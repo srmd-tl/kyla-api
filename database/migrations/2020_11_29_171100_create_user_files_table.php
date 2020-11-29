7c0b0738decaed1b0d986be0730e2c2b57951574
Sarmad Sohail <sarmad-sohail@hotmail.com>
2020-11-29 22:16:17 +0500

    user files migration

---
+++ database/migrations/2020_11_29_171100_create_user_files_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_files', function (Blueprint $table) {
+            $table->id();
+            $table->string("path");
+            $table->tinyInteger("type")->comment("1 for photo, 2 for video");
+            $table->foreignId("user_id");
+            $table->timestamps();
+            //Foreign Keys
+            $table->foreign("user_id")
+                ->references("id")
+                ->on("users")
+                ->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_files');
+    }
+}
