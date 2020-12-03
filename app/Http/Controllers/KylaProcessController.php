27beea511da12cc5faa6698f35feb242e4e873d9
Sarmad Sohail <sarmad-sohail@hotmail.com>
2020-12-03 22:26:47 +0500

    updates

--- app/Http/Controllers/KylaProcessController.php
+++ app/Http/Controllers/KylaProcessController.php
@@ -38,7 +38,10 @@ class KylaProcessController extends Controller
             "officer_name" => request()->officerName,
             "officer_number" => request()->officerNumber,
             "location" => request()->location,
-            "law" => request()->law
+            "law" => request()->law,
+            "alert_via_sms" => request()->viaSms ?? 0,
+            "alert_via_email" => request()->viaEmail ?? 0,
+            "user_id" => auth()->user()->id
         ]);
         return response()->success("Info saved");
     }
