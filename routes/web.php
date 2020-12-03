f72aaf57423d683cb9235e839613a3a6da6c434f
Sarmad Sohail <sarmad-sohail@hotmail.com>
2020-12-03 23:42:55 +0500

    report

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    \App\Utils\Helper::sendMessage("ello","+923315743763");
     return view('welcome');
 });
-Route::get('/test',[\App\Http\Controllers\UserFileController::class,'audioStore']);
+Route::get('/kylaProcess/{kylaProcess}', [\App\Http\Controllers\KylaProcessController::class, 'show'])
+    ->name('kylaProcess.show');
